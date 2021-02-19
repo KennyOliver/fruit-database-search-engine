437a70d5327da6e5592afbcd956eceb84961e3f4
Kenny O <kenny_oliver@icloud.com>
2021-02-19 20:33:05 +0000

    [+] Editing whitespace and comments

--- main.py
+++ main.py
@@ -1,42 +1,29 @@
 def file_search(to_search):
   file = open("fruits.txt",'r')
-  
-  print("<-- Results -->")
-  
+  print("<-- RESULTS -->")
   for record in file:
     if to_search in record:
       print(record)
-  
   print("=" * 20)
-  
   file.close()
 #====================
 def get_all():
   file = open("fruits.txt",'r')
-  
-  print("<-- Print All -->")
-  
+  print("<-- RESULTS: Print All -->")
   for record in file:
     print(record)
-  
   print("=" * 20)
-  
   file.close()
 #====================
 def letter_search(letter):
   file = open("stuff.txt",'r')
-  
-  print(f"<-- {letter} -->".format(letter))
-  
+  print(f"<-- RESULTS for {letter} -->".format(letter))
   for record in file:
     if record.startswith(letter):
       print(record)
-  
   print("=" * 20)
-  
   file.close()
-
-
+#====================
 # MAIN PROGRAM
 print("\nFruit Search")
 print("\t[*] Retrieve all\n","\t[%] Letter category\n","\t[!] Normal search")
